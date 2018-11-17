c628df39f1b874bb1143150db3e51e9ed7e53922
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-17 13:56:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  var i;
   if(katzDeliLine.length === 0){
     return ("The line is currently empty.");
   } else {
@@ -26,5 +25,6 @@ function currentLine(katzDeliLine){
         lineList = lineList + ", " + (i+1) + ". " + (katzDeliLine[i]);
       }
     }
+    return lineList;
   }
 }
