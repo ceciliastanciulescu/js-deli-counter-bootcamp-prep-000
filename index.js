12a7e9eda1946ce6272e64a1f398c7702698b0ae
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-17 13:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function takeANumber(katzDeliLine, newPerson){
   return "Welcome, "+ newPerson + ". You are number " + katzDeliLine.length + " in line."
 }
 
+function nowServing(katzDeliLine){
+  
+}
