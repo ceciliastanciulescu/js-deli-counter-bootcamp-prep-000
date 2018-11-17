4f0ee05d9412f21c553678a51a836b4b69f9c2e0
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-17 13:46:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
     return ("The line is currently empty.");
   } else {
-    katzDeliLine.toString();
-    return ("The line is currently: " + katzDeliLine + ".");
+    katzDeliLine[0].toString();
+    return ("The line is currently: " + katzDeliLine[0] + ".");
   }
 }
