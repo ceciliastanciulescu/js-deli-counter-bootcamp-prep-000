abadc29b41fa675da632d4d881c489e18d5e35bb
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-17 13:14:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 function takeANumber(katzDeli, otherDeli){
-  for(var i = 1; i < katzDeli.length; i++){
-    katzDeli.push("Ada");
-  }
+  katzDeli.push("Ada");
   return "Welcome," + katzDeli[i]. + "You are number 1 in line."
 }
 
-/*
-function theBeatlesPlay(musicians, instruments) {
-  var array = [musicians, instruments];
-    for (let i = 0; musicians.length; i++) {
-      array.push(musicians[i]+"plays" + "instruments[i]")}
-      return array }
-*/
